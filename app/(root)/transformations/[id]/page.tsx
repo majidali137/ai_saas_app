dec652c31b2e7175b86df06ad7fe272a6d457dc0
majidali137 <majidaliwp69@gmail.com>
2024-08-03 10:32:29 +0500

    added rooting base structure folders

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>Transformations Page</div>
+  )
+}
+
+export default TransformationsPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransformationsPage = () => {
+  return (
+    <div>Update Transformations Page</div>
+  )
+}
+
+export default UpdateTransformationsPage
