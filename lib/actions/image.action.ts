091278c60de086545ae22a040abc07389c111311
majidali137 <majidaliwp69@gmail.com>
2024-08-10 10:16:34 +0500

    Some errors have been removed

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -17,74 +17,77 @@ const populateUser = (query: any) => query.populate({
 // Add Image
 export async function addImage({ image, userId, path }: AddImageParams) {
     try {
-        await connectToDatabase()
-
-        const author = await User.findById(userId)
-
-        if (!author) {
-            throw new Error("User not found")
-        }
-
-        const newImage = await Image.create({
-            ...image,
-            author: author._id,
-        })
-
-        revalidatePath(path)
-        return JSON.parse(JSON.stringify(newImage));
+      await connectToDatabase();
+  
+      const author = await User.findById(userId);
+  
+      if (!author) {
+        throw new Error("User not found");
+      }
+  
+      const newImage = await Image.create({
+        ...image,
+        author: author._id,
+      })
+  
+      revalidatePath(path);
+  
+      return JSON.parse(JSON.stringify(newImage));
     } catch (error) {
-        handleError(error);
+      handleError(error)
     }
-}
+  }
+  
 
 
 // update Image
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
     try {
-        await connectToDatabase()
-
-        const imageToUpdate = await Image.findById(image._id)
-
-        if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
-            throw new Error("Unauthorized or  Image not found")
-        }
-
-        const updatedImage = await Image.findByIdAndUpdate(
-            imageToUpdate._id,
-            image,
-            { new: true }
-        )
-
-        revalidatePath(path)
-        return JSON.parse(JSON.stringify(updatedImage));
+      await connectToDatabase();
+  
+      const imageToUpdate = await Image.findById(image._id);
+  
+      if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+        throw new Error("Unauthorized or image not found");
+      }
+  
+      const updatedImage = await Image.findByIdAndUpdate(
+        imageToUpdate._id,
+        image,
+        { new: true }
+      )
+  
+      revalidatePath(path);
+  
+      return JSON.parse(JSON.stringify(updatedImage));
     } catch (error) {
-        handleError(error);
+      handleError(error)
     }
-}
+  }
 
 // Delete Image
 export async function deleteImage(imageId: string) {
     try {
-        await connectToDatabase()
-
-        await Image.findByIdAndDelete(imageId)
+      await connectToDatabase();
+  
+      await Image.findByIdAndDelete(imageId);
     } catch (error) {
-        handleError(error);
-    } finally {
-        redirect("/")
+      handleError(error)
+    } finally{
+      redirect('/')
     }
-}
+  }
 // Get Image
 export async function getImageById(imageId: string) {
     try {
-        await connectToDatabase()
-
-        const image = await populateUser(Image.findById(imageId))
-
-        if (!image) throw new Error("Image not found")
-
-        return JSON.parse(JSON.stringify(image));
+      await connectToDatabase();
+  
+      const image = await populateUser(Image.findById(imageId));
+  
+      if(!image) throw new Error("Image not found");
+  
+      return JSON.parse(JSON.stringify(image));
     } catch (error) {
-        handleError(error);
+      handleError(error)
     }
-}
+  }
